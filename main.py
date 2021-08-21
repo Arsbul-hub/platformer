5a685241128c04920d7a1cec454d75d2b3c39b6a
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2021-08-21 09:02:58 +0300

    управление + регулирование колизий из размеров блоков

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class App():
 
         self.l = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -20,7 +20,7 @@ class App():
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -39,15 +39,15 @@ class App():
 #
         self.sc = display.set_mode((700,700))
 
-        self.y = 0
+        self.y = 50
         self.x = 0
         self.colliders = []
-
+        self.size = (100,100)
         while True:
             self.update()
 
     def update(self):
-        time.sleep(1/70)
+        time.sleep(1/100)
         self.sc.fill((0,0,0))
         for e in event.get(): # Обрабатываем события
             if e.type == QUIT:
@@ -55,30 +55,30 @@ class App():
         for i in range(30):
             for j in range(30):
                 if self.l[i][j] == 1:
-                    draw.rect(self.sc, (255, 255, 255),(j*50-self.x,i*50-self.y,50,50))
+                    draw.rect(self.sc, (255, 255, 255),(j*self.size[0]-self.x,i*self.size[1]-self.y,self.size[0],self.size[1]))
 
         #print(int(self.x/50)+6*50)
 
 
 
         try:
-            draw.rect(self.sc, (0, 255, 0), (350 ,350, 50, 50))
+            draw.rect(self.sc, (0, 255, 0), (350 ,350, self.size[0], self.size[1]))
             #draw.rect(self.sc, (0, 255, 0), (int(self.y/50)*50+7*50, int(self.x/50)*50+7*50 , 50, 50))
-            print(int((self.y)/50)+7,int((self.x)/50)+7, "item1: ",self.l[int((self.y+350)/50) ][int((self.x+349)/50)],self.l[int((self.y+399)/50) ][int((self.x+349)/50)])
+            #print(int((self.y)/50)+7,int((self.x)/50)+7, "item1: ",self.l[int((self.y+350)/50) ][int((self.x+349)/50)],self.l[int((self.y+399)/50) ][int((self.x+349)/50)])
             #self.l[int((self.y) / 50) + 6][int(self.x / 50) + 6] = 9
             #print(self.l)
             #for i in self.l:
             #    print(i,"\n")aaaaaaaaa
         except:
             print("error")
-        #print(int((self.y)/50)+7,int((self.x)/50)+7)
-        if keyboard.is_pressed("s") and self.l[int((self.y+400)/50) ][int((self.x+350)/50)] != 1 and self.l[int((self.y+400)/50) ][int((self.x+399)/50)] != 1:
+        print(self.x,self.y)
+        if keyboard.is_pressed("s") and self.l[int((self.y+350+self.size[1])/self.size[1]) ][int((self.x+350)/self.size[0])] != 1 and self.l[int((self.y+350+self.size[1])/self.size[1]) ][int((self.x+349+self.size[0])/self.size[0])] != 1:
             self.y += 1
-        if keyboard.is_pressed("w") and self.l[int((self.y+349)/50) ][int((self.x+350)/50)] != 1 and self.l[int((self.y+349)/50) ][int((self.x+399)/50)] != 1:
+        if keyboard.is_pressed("w") and self.l[int((self.y+349)/self.size[1]) ][int((self.x+350)/self.size[0])] != 1 and self.l[int((self.y+349)/self.size[1]) ][int((self.x+349+self.size[1])/self.size[0])] != 1:
             self.y -=1
-        if keyboard.is_pressed("d") and self.l[int((self.y+350)/50) ][int((self.x+400)/50)] != 1 and self.l[int((self.y+399)/50) ][int((self.x+400)/50)] !=1:
+        if keyboard.is_pressed("d") and self.l[int((self.y+350)/self.size[1]) ][int((self.x+350+self.size[0])/self.size[0])] != 1 and self.l[int((self.y+349+self.size[1])/self.size[1]) ][int((self.x+350+self.size[0])/self.size[0])] !=1:
             self.x += 1
-        if keyboard.is_pressed("a") and self.l[int((self.y+350)/50) ][int((self.x+349)/50)] != 1 and self.l[int((self.y+399)/50) ][int((self.x+349)/50)] != 1:
+        if keyboard.is_pressed("a") and self.l[int((self.y+350)/self.size[1]) ][int((self.x+349)/self.size[1])] != 1 and self.l[int((self.y+349+self.size[1])/self.size[1]) ][int((self.x+349)/self.size[0])] != 1:
             self.x -= 1
         display.update()
 App()
