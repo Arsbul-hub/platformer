45efafc47f2632f74b998fbefdc700d748552c1b
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2021-08-27 22:04:01 +0300

    button control test

--- main.py
+++ main.py
@@ -43,8 +43,23 @@ class App():
         self.x = 0
         self.colliders = []
         self.size = (100,100)
-        self.test = Button(self.sc,path="1.png",pos=(100,100),command=self.tt)
+        #self.test = Button(self.sc,path="1.png",pos=(100,100),command=self.tt)
+
+        self.main_lavels = Button(self.sc,size=(100,50), path="1.png",text="hi", pos=(300, 325))
+        self.step = 1
+        self.left = 0
+        self.right = 0
         while True:
+            for e in event.get():  # Обрабатываем события
+                if e.type == QUIT:
+                    quit()
+                self.main_lavels.update(e)
+            self.main_lavels.update_show()
+            if self.main_lavels.get_value():
+                break
+            display.update()
+        while True:
+
             #print(event.get())
 
 
@@ -52,7 +67,7 @@ class App():
             self.update()
             display.update()
 
-    def tt(self):
+    def to_main_lavels(self):
         print(123)
     def update(self):
         #time.sleep(1/100)
@@ -67,36 +82,83 @@ class App():
         for e in event.get():  # Обрабатываем события
             if e.type == QUIT:
                 quit()
+            elif e.type == KEYDOWN:
+                if e.key == K_a:
+                    self.right = 0
+                    self.left +=1
+                elif e.key == K_d:
+                    self.left = 0
+                    self.right +=1
             #self.test.update_show()
-            self.test.update(e)
-        self.test.update_show()
+
 
         #print(int(self.x/50)+6*50)
 
 
-
+        if self.left == 5:
+                self.left = 1
+        if self.right == 5:
+                self.right = 1
         try:
             draw.rect(self.sc, (0, 255, 0), (350 ,350, self.size[0], self.size[1]))
-            if keyboard.is_pressed("s") and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
-                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
-                int((self.x + 349 + self.size[0]) / self.size[0])] != 1:
-                self.y += 1
-            if keyboard.is_pressed("w") and self.l[int((self.y + 349) / self.size[1])][
-                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 349) / self.size[1])][
-                int((self.x + 349 + self.size[1]) / self.size[0])] != 1:
-                self.y -= 1
-            if keyboard.is_pressed("d") and self.l[int((self.y + 350) / self.size[1])][
-                int((self.x + 350 + self.size[0]) / self.size[0])] != 1 and \
-                    self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
-                        int((self.x + 350 + self.size[0]) / self.size[0])] != 1:
-                self.x += 1
-            if keyboard.is_pressed("a") and self.l[int((self.y + 350) / self.size[1])][
-                int((self.x + 349) / self.size[1])] != 1 and self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
-                int((self.x + 349) / self.size[0])] != 1:
-                self.x -= 1
+            #if keyboard.is_pressed("s") and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+            #    int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+            #    int((self.x + 349 + self.size[0]) / self.size[0])] != 1:
+            #    self.hor = 1
+            #if keyboard.is_pressed("w") and self.l[int((self.y + 349) / self.size[1])][
+                #int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 349) / self.size[1])][
+                #int((self.x + 349 + self.size[1]) / self.size[0])] != 1:
+                #self.y -= 1
+            #if keyboard.is_pressed("d") and self.l[int((self.y + 350) / self.size[1])][
+                #int((self.x + 350 + self.size[0]) / self.size[0])] != 1 and \
+                #    self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
+                #        int((self.x + 350 + self.size[0]) / self.size[0])] != 1:
+                ##self.x += 1
+
+
+
         except:
             pass
 
+        if self.left == 1 and self.l[int((self.y + 350) / self.size[1])][
+                int((self.x + 349) / self.size[1])] != 1 and self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
+                int((self.x + 349) / self.size[0])] != 1:
+            self.x -= 1
+        if self.left == 2 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+                int((self.x + 349 + self.size[0]) / self.size[0])] != 1:
+            self.y += 1
+        if self.left == 3 and self.l[int((self.y + 350) / self.size[1])][
+                int((self.x + 350 + self.size[0]) / self.size[0])] != 1 and \
+                    self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
+                        int((self.x + 350 + self.size[0]) / self.size[0])] != 1:
+            self.x+=1
+        if self.left == 4 and self.l[int((self.y + 349) / self.size[1])][
+                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 349) / self.size[1])][
+                int((self.x + 349 + self.size[1]) / self.size[0])] != 1:
 
+            self.y -=1
+
+
+        if self.right == 1 and self.l[int((self.y + 349) / self.size[1])][
+                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 349) / self.size[1])][
+                int((self.x + 349 + self.size[1]) / self.size[0])] != 1:
+            self.y -=1
+        if self.right == 2 and self.l[int((self.y + 350) / self.size[1])][
+                int((self.x + 350 + self.size[0]) / self.size[0])] != 1 and \
+                    self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
+                        int((self.x + 350 + self.size[0]) / self.size[0])] != 1:
+            self.x += 1
+        if self.right == 3 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+                int((self.x + 350) / self.size[0])] != 1 and self.l[int((self.y + 350 + self.size[1]) / self.size[1])][
+                int((self.x + 349 + self.size[0]) / self.size[0])] != 1:
+            self.y+=1
+        if self.right == 4 and self.l[int((self.y + 350) / self.size[1])][
+                int((self.x + 349) / self.size[1])] != 1 and self.l[int((self.y + 349 + self.size[1]) / self.size[1])][
+                int((self.x + 349) / self.size[0])] != 1:
+
+            self.x -=1
+
+        print(self.right)
 
 App()
